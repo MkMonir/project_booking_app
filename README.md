22859c28496193cbb9b33f07a7e11e853185d124
MkMonir <khanmonir1335@gmail.com>
2022-05-26 22:25:35 +0600

    moving files

--- README.md
+++
@@ -1,1 +0,0 @@
-# MERN stack hotel reservation app project. React Node.js MongoDB full-stack app using Context API, JWT, custom hooks, and React calendar.
--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  console.log(`App successfully running on port ${port}!`);
-});

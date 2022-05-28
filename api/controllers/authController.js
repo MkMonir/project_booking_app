4fa8e49e6d21ae8fa409cfa6b56048bbac10a12d
MkMonir <khanmonir1335@gmail.com>
2022-05-28 20:15:22 +0600

    create register and login functionility

---
+++ api/controllers/authController.js
@@ -0,0 +1,59 @@
+import jwt from 'jsonwebtoken';
+import catchAsync from './../utils/catchAsync';
+import AppError from './../utils/appError';
+import User from './../models/userModel';
+
+const signToken = (id, isAdmin) =>
+  jwt.sign({ id, isAdmin }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id, user.isAdmin);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // REMOVE THE PASSWORD FROM THE OUTPUT
+  user.password = undefined;
+  user.isAdmin = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+export const register = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  createSendToken(user, 201, res);
+});

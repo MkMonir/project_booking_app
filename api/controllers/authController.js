7055633feed77e3c6c4c8ec186eb285108d383ce
MkMonir <khanmonir1335@gmail.com>
2022-05-29 21:34:26 +0600

    verifying authentication and user roles

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import catchAsync from './../utils/catchAsync';
 import AppError from './../utils/appError';
@@ -20,7 +21,6 @@ const createSendToken = (user, statusCode, res) => {
 
   // REMOVE THE PASSWORD FROM THE OUTPUT
   user.password = undefined;
-  user.isAdmin = undefined;
 
   res.status(statusCode).json({
     status: 'success',
@@ -37,6 +37,7 @@ export const register = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    isAdmin: req.body.isAdmin,
   });
 
   createSendToken(newUser, 201, res);
@@ -57,3 +58,43 @@ export const login = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 201, res);
 });
+
+export const protect = catchAsync(async (req, res, next) => {
+  // 1) note: GETTING TOKEN AND CHECK IF IT'S THERE
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access', 401));
+  }
+
+  // 2) note: VERIFICATION TOKEN
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) note: CHECK IF USER STILL EXIST
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(new Error('The user belonging to this token is no longer exist', 401));
+  }
+
+  // 4) note: CHEK ID USER CHANGE PASSWORD AFTER THE TOKEN WAS ISSUED
+  if (currentUser.changePasswordAfter(decoded.iat)) {
+    return next(new Error('User recently changed password! Please log in again!'), 401);
+  }
+
+  // note: GRANT ACCESS TO PROTECTED DATA
+  req.user = currentUser;
+  next();
+});
+
+export const restrictTo = (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(new AppError('You do not have permission to perform this action', 403));
+  }
+  next();
+};

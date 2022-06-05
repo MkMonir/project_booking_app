55e26e0934e9594456751965bdd236c84e2b17f6
MkMonir <khanmonir1335@gmail.com>
2022-06-05 22:41:14 +0600

    implementing api features

--- api/controllers/handlerFactory.js
+++ api/controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 import catchAsync from './../utils/catchAsync';
 import AppError from './../utils/appError';
+import APIFeatures from './../utils/apiFeatures';
 
 export const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -62,7 +63,17 @@ export const getOne = (Model) =>
 
 export const getAll = (Model) =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.find();
+    // note: To allow for nested GET reviews on tour (hack)
+    let filter = {};
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    // const doc = await features.query.explain();
+    const doc = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({

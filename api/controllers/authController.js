3e8d82ccb5db10a2bd0d1b6adda95bbce783fe71
MkMonir <khanmonir1335@gmail.com>
2022-06-26 20:39:03 +0600

    getting jwt token from headers and protect routes

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -14,6 +14,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'none',
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
@@ -25,35 +26,27 @@ const createSendToken = (user, statusCode, res) => {
   res.status(statusCode).json({
     status: 'success',
     token,
-    data: {
-      user,
-    },
+    data: { user },
   });
 };
 
 export const register = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    isAdmin: req.body.isAdmin,
-  });
+  const newUser = await User.create(req.body);
 
   createSendToken(newUser, 201, res);
 });
 
 export const login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+  if (!username || !password) {
+    return next(new AppError('Please provide username and password!', 400));
   }
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ username }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('Incorrect username or password', 401));
   }
 
   createSendToken(user, 201, res);
@@ -62,8 +55,8 @@ export const login = catchAsync(async (req, res, next) => {
 export const protect = catchAsync(async (req, res, next) => {
   // 1) note: GETTING TOKEN AND CHECK IF IT'S THERE
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
+  if (req.headers.token) {
+    token = req.headers.token;
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }

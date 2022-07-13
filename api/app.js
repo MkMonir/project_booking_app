ecdcebd68cb9e32de19a0d8df0a8f75488740120
MkMonir <khanmonir1335@gmail.com>
2022-07-14 00:42:14 +0600

    routes and middlewares

---
+++ api/app.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import multer from 'multer';
+import authRouter from './routes/authRoutes';
+import usersRouter from './routes/usersRoutes';
+import hotelsRouter from './routes/hotelsRoutes';
+import roomsRouter from './routes/roomsRoutes';
+
+import AppError from './utils/appError';
+import globalErrorHandler from './controllers/errorController';
+
+const app = express();
+
+dotenv.config({ path: './config.env' });
+
+const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+// note: Routes Middleware
+// app.use('/');
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/hotels', hotelsRouter);
+app.use('/api/v1/rooms', roomsRouter);
+
+// Global error handling middleware
+app.all('*', (req, res, next) => {
+  return next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+export default app;

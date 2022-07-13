6d45b0c3c494908fc3ab4c1ef54e9c3d0a547907
MkMonir <khanmonir1335@gmail.com>
2022-07-14 00:42:59 +0600

    upload files using multer

--- api/app.js
+++ api/app.js
@@ -33,6 +33,21 @@ app.use(
   })
 );
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
 // note: Routes Middleware
 // app.use('/');
 app.use('/api/v1/auth', authRouter);
